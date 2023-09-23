651419be21fb870f9af188e1c838f55e631f79c6
Manuelleon123 <manuel.j.leon.c2@gmail.com>
2023-09-23 17:41:12 -0400

    feat: finish game

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # rock-paper-scissors-odin-23sep23
-# rock-paper-scissors-odin-23sep23
---
+++ main.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    const cPselection = ['rock','paper','scissors'];
+
+    const random = Math.floor(Math.random() * cPselection.length);
+}
+
+function game() {
+    const playerSelection = prompt("Please chose beetween 'rock', 'paper'or 'rock')
+
+}
+
+}else if (
+    playerSelection == 'scissor' &&
+    computerSelection == 'paper'
+)
+
+if 
